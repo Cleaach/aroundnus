7039016ab53e3ec08432a503d3c77871a766486b
Dylan Astono <dylan.ananda@gmail.com>
2025-06-02 01:40:01 +0800

    Minor tweaks to icons

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,10 +1,30 @@
-import { Tabs } from 'expo-router';
+import { Tabs } from "expo-router";
+import { IconSymbol } from "../../components/ui/IconSymbol";
+import { useColorScheme } from "../../hooks/useColorScheme";
 
 export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Navigation' }} />
-      <Tabs.Screen name="explore" options={{ title: 'Social' }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Navigation",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol name="house.fill" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Social",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol name="person.fill" color={color} size={24} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
