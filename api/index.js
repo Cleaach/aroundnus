d9fa986f16ee4e25466133861742f58a8d18f802
Dylan Astono <dylan.ananda@gmail.com>
2025-06-25 16:31:58 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
-module.exports = (req, res) => {
-    res.status(200).json({ message: "Hello from Vercel API!" });
-};
+const serverlessExpress = require('@vendia/serverless-express');
+const app = require('../backend/index');
+
+module.exports = serverlessExpress({ app });
