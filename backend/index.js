c2cf22a4612ba1b0a95b76ed7c6df0c0a13795cd
Dylan Astono <dylan.ananda@gmail.com>
2025-06-25 17:21:05 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const savedLocationsRoutes = require('../routes/savedLocationsRoutes');
-const authRoutes = require('../routes/authRoutes');
-const profilePictureRoutes = require('../routes/profileRoutes');
+const savedLocationsRoutes = require('./routes/savedLocationsRoutes');
+const authRoutes = require('./routes/authRoutes');
+const profilePictureRoutes = require('./routes/profileRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,7 +18,9 @@ app.use((req, res) => {
     res.status(404).json({ error: 'Not Found', path: req.originalUrl });
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
 
