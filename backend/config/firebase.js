444db446c9f70488b7061c30bda5cd0627beac0d
Dylan Astono <dylan.ananda@gmail.com>
2025-06-25 17:58:51 +0800

    configured the path for the serviceaccountkey

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,12 +1,12 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../config/serviceAccountKey.json');
+const serviceAccount = require('/etc/secrets/serviceAccountKey.json');
 
 try {
   admin.app();
 } catch (e) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: 'aroundnus-fa582.firebasestorage.app', 
+    storageBucket: 'aroundnus-fa582.firebasestorage.app',
   });
 }
 
