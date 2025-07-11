5f50233e92180133705e68a7ebae63f8b30489b5
Dylan Astono <dylan.ananda@gmail.com>
2025-07-11 19:50:58 +0800

    Added a savedLocations feature

--- backend/controllers/savedLocationsController.js
+++ backend/controllers/savedLocationsController.js
@@ -5,8 +5,8 @@ const getSavedLocations = async (req, res) => {
     const { uid } = req.user;
     const userDoc = await admin.firestore().collection('users').doc(uid).get();
     const userData = userDoc.data();
-    const savedLocations = userData.savedLocations || [];
-    res.status(200).json(savedLocations);
+    const bookmarkedLocations = userData.bookmarkedLocations || [];
+    res.status(200).json(bookmarkedLocations);
 }
 
 const addSavedLocation = async (req, res) => {
@@ -15,11 +15,11 @@ const addSavedLocation = async (req, res) => {
 
     const userDoc = await admin.firestore().collection('users').doc(uid).get();
     const userData = userDoc.data();
-    const savedLocations = userData.savedLocations || [];
+    const bookmarkedLocations = userData.bookmarkedLocations || [];
 
-    savedLocations.push(location);
+    bookmarkedLocations.push(location);
 
-    await admin.firestore().collection('users').doc(uid).update({ savedLocations });
+    await admin.firestore().collection('users').doc(uid).update({ bookmarkedLocations: bookmarkedLocations });
     res.status(200).json({ message: "Location added to saved locations" });
 }
 
@@ -29,11 +29,11 @@ const deleteSavedLocation = async (req, res) => {
 
     const userDoc = await admin.firestore().collection('users').doc(uid).get();
     const userData = userDoc.data();
-    const savedLocations = userData.savedLocations || [];
+    const bookmarkedLocations = userData.bookmarkedLocations || [];
 
-    const newSavedLocations = savedLocations.filter(loc => loc.id !== location.id);
+    const newBookmarkedLocations = bookmarkedLocations.filter(loc => loc.id !== location.id);
 
-    await admin.firestore().collection('users').doc(uid).update({ savedLocations: newSavedLocations });
+    await admin.firestore().collection('users').doc(uid).update({ bookmarkedLocations: newBookmarkedLocations });
     res.status(200).json({ message: "Location deleted from saved locations" });
 }
 
